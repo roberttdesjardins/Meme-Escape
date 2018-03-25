d758a53fd3ec26e6d3f8d4e619df20122a0a0618
Robert Desjardins <roberttdesjardins@gmail.com>
2018-03-24 17:30:16 -0700

    first commit

---
+++ SlingshotTests/SlingshotTests.swift
@@ -0,0 +1,36 @@
+//
+//  SlingshotTests.swift
+//  SlingshotTests
+//
+//  Created by Robert Desjardins on 2018-03-24.
+//  Copyright © 2018 Robert Desjardins. All rights reserved.
+//
+
+import XCTest
+@testable import Slingshot
+
+class SlingshotTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
